b89678303fc28913f409deeb22514fe719873af3
rizkysafdila <rizkysafdila@gmail.com>
2024-09-28 15:18:08 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+const runtimeConfig = useRuntimeConfig()
+const appDomain = runtimeConfig.public.appUrl
+const appName = appConfig.name
+const appDescription = appConfig.description
+const appImage = `${appDomain}/logo.svg`
+
+useSeoMeta({
+  title: appName,
+  ogTitle: appName,
+  description: appDescription,
+  ogDescription: appDescription,
+  ogImage: appImage,
+  twitterCard: 'summary_large_image',
+})
+
+useHead({
+  bodyAttrs: {
+    'data-theme': 'morpheme',
+  },
+})
+</script>
+
+<template>
+  <Html lang="en" />
+  <Head>
+    <Link rel="icon" href="/logo.png" type="image/png" />
+  </Head>
+  <NuxtLayout>
+    <NuxtLoadingIndicator />
+    <NuxtPage />
+  </NuxtLayout>
+  <GlobalToast />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,56 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    ['@pinia/nuxt', { disableVuex: true }],
+    '@vueuse/nuxt',
+    '@morpheme/nuxt',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    // uncomment to enable storybook
+    // '@nuxtjs/storybook',
+    '@nuxt/test-utils/module',
+    '@nuxt/fonts',
+    '@pinia-plugin-persistedstate/nuxt',
+  ],
+
+  runtimeConfig: {
+    public: {
+      gaId: '',
+      apiUrl: '',
+      appUrl: '',
+    },
+  },
+
+  imports: {
+    dirs: ['./api', './stores'],
+  },
+
+  // https://gitsindonesia.github.io/ui-component/
+  morpheme: {
+    darkMode: true,
+    css: false,
+    sass: true,
+    theme: 'morpheme',
+    transpileDeps: false,
+    optimizeDeps: false,
+  },
+
+  devtools: {
+    enabled: true,
+  },
+
+  css: ['~/assets/css/app.css'],
+
+  components: [
+    { path: '~/components/app', pathPrefix: false },
+    { path: '~/components/ui', pathPrefix: false },
+
+    // It's important that this comes last if you have overrides you wish to apply
+    // to sub-directories of `~/components`.
+    //
+    // ~/components/Btn.vue => <Btn />
+    // ~/components/base/Btn.vue => <BaseBtn />
+    '~/components',
+  ],
+})
